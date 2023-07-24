0b5144b4543fd6482b76ae971e65bb8fc55582bd
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-07-24 23:56:01 +0530

    Added chat Bot functionality specific to the page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,7 @@ const Header = () => {
 
   return (
     <div className="header">
+        
         <img src={Logo} alt="" className="logo" />
         {(menuOpened === false && mobile===true) ? 
         (<div
