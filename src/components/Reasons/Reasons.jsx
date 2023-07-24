978304e24d6bc342d77613ba29ac99b0c6c459e0
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-07-24 11:28:10 +0530

    Reasons section was styled using grid

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Reasons.css";
 import image1 from "../../assets/image1.png";
-import image2 from "../../assets/image2.png";
+import image2 from "../../assets/image2.jpg";
 import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
 import nb from '../../assets/nb.png';
@@ -25,24 +25,34 @@ const Reasons = () => {
                 <span className='stroke-text'>why</span>
                 <span> choose us?</span>
             </div>
-                <div className="details-r" >
-                    <div>
-                        <img src={tick} alt=""></img>
-                        <span></span>
-                    </div>
-                    <div>
-                        <img src={tick} alt=""></img>
-                        <span></span>
-                    </div>
-                    <div>
-                        <img src={tick} alt=""></img>
-                        <span></span>
-                    </div>
-                    <div>
-                        <img src={tick}  alt=""></img>
-                        <span></span>
-                    </div>
+            
+            <div className="details-r" >
+                <div>
+                    <img src={tick} alt=""></img>
+                    <span>OVER 800+ TRUSTED FOLLOWERS</span>
                 </div>
+                <div>
+                    <img src={tick} alt=""></img>
+                    <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+                </div>
+                <div>
+                    <img src={tick} alt=""></img>
+                    <span>1 WEEK FREE INTERACTION FOR NEW MEMBER</span>
+                </div>
+                <div>
+                    <img src={tick}  alt=""></img>
+                    <span>GOOD QUALITY EQUIPMENT AND COMMUNITY</span>
+                </div>
+            </div>
+            <span style={{
+                color: "var(--gray)",
+                fontWeight: "normal",
+            }}>OUR PARTNERS</span>
+            <div className="partners">
+                <img src={nb} alt="" />
+                <img src={adidas} alt="" />
+                <img src={nike} alt="" />
+            </div>
         </div>
     </div>
   );
